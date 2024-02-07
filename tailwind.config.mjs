e836edfceccfd30e8a178d45259028d8246ce295
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-07 16:03:52 -0300

    latest changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-	'./node_modules/flowbite/**/*.js'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		
 	],
 }

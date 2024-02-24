4fe17de3cc5641b8e180454dc2c998b99640ff3e
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-24 16:45:14 -0300

    change pre-render

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro"
-
+export const prerender = true;
 export const POST: APIRoute = async ({ request }) => {
    const data = await request.formData();
    const name = data.get("name")?.toString();

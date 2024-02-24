3768fba1ea1f4e05ec1527ce7dcd965ab4468776
RadamantiumX <82120092+RadamantiumX@users.noreply.github.com>
2024-02-24 16:49:39 -0300

    change pre-render to false

--- src/pages/api/feedback.ts
+++ src/pages/api/feedback.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from "astro"
-export const prerender = true;
+export const prerender = false;
 export const POST: APIRoute = async ({ request }) => {
    const data = await request.formData();
    const name = data.get("name")?.toString();
